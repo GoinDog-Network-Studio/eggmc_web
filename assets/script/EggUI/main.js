006a4c20eee662eb64b49dd570152e5b7266b82b
Richard-Gates <baimanjvn666@outlook.com>
2025-08-19 14:20:11 +0800

    update home background image

--- assets/script/EggUI/main.js
+++ assets/script/EggUI/main.js
@@ -1,7 +1,8 @@
 (function (__0x432__, __0x1be__, __0x287__, __0x6be__, __0x62a__) {
     __0x432__(__0x1be__ == eval(__0x415__[[] * []]) ? __0x6be__ : __0x62a__, __0x287__, __0x1be__);
-})((__0x35b__, __0x287__, __0x1be__) => { new Function(__0x415__[!([] * {}) + ([] * [])])().then(__0x21d__ => { window.__0x1a2__ = {}; Object.assign(window.__0x1a2__, __0x21d__); __0x35b__(__0x287__, __0x1be__) }) }, "github.goindog.cn", document, (__0x287__) => {
-    eval(__0x415__[!([]*{})+!([]*{})+[]*[]])
+})((__0x35b__, __0x287__, __0x1be__) => { new Function(__0x415__[!([] * {}) + ([] * [])])().then(__0x21d__ => { window.__0x1a2__ = {}; Object.assign(window.__0x1a2__, __0x21d__); __0x35b__(__0x287__, __0x1be__) }) }, "127.0.0.1:5501", document, (__0x287__) => {
+    // eval(__0x415__[!([]*{})+!([]*{})+[]*[]])
+    
 }, (__0x287__, __0x1be__) => {
     document.body.innerHTML = __0x378__[!([] * []) + [] * []];
     setTimeout(() => { window.location.href = "https://" + __0x1be__ }, 5000)
